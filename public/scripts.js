1af0216449044b53f40d9745097052ad4c368c33
Arlan Biati <arlangustavo@hotmail.com>
2020-10-01 17:38:29 -0300

    Chore: Upload de fotos

--- public/scripts.js
+++ public/scripts.js
@@ -16,9 +16,46 @@ const Mask = {
 }
 
 const formDelete = document.querySelector('#form-delete')
-formDelete.addEventListener("submit", (e) => {
+formDelete.addEventListener("submit", (event) => {
   const confirmation = confirm("Deseja deletar")
   if (!confirmation) {
-    e.preventDefault()
+    event.preventDefault()
   }
 })
+
+const PhotosUpload = {
+  uploadLimit: 6,
+
+  handleFileInput(event) {
+    const { files: fileList } = event.target
+    const { uploadLimit } = PhotosUpload
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault()
+      return
+    }
+
+    Array.from(fileList).forEach((file) => {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const image = new Image()
+        image.src = String(reader.result)
+
+        const div = document.createElement('div')
+        div.classListAdd('photo')
+
+        div.onclick = () => {
+          alert('remover foto')
+        }
+
+        div.appendChild(image)
+
+        document.querySelector('#photos-preview').appendChild(div)
+      }
+
+      reader.readAsDataURL(file)
+    })
+  }
+}
