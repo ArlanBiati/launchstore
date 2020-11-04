d178b1cd5a3eede757cf7d93d759b31e9d682c7a
Arlan Biati <arlangustavo@hotmail.com>
2020-11-03 21:18:34 -0300

    Chore: página de compras

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,7 +2,7 @@ const Category = require('../models/Category')
 const Product = require('../models/Product')
 const File = require('../models/File')
 
-const { formatPrice } = require('../../lib/utils')
+const { formatPrice, date } = require('../../lib/utils')
 
 module.exports = {
   async create(req, res) {
@@ -46,6 +46,28 @@ module.exports = {
     await Promise.all(filesPromise)
 
     return res.redirect(`products/${productId}/edit`)
+  },
+
+  async show(req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if (!product) {
+      return res.send("Produto não encontrado")
+    }
+
+    const { day, month, hour, minutes } = date(product.updated_at)
+
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}h${minutes}`
+    }
+
+    product.oldPrice = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+
+    return res.render('products/show', { product })
   },
 
   async edit(req, res) {
@@ -105,7 +127,7 @@ module.exports = {
 
     await Product.update(req.body)
 
-    return res.redirect(`/products/${req.body.id}/edit`)
+    return res.redirect(`/products/${req.body.id}`)
   },
 
   async delete(req, res) {
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,16 +4,24 @@ module.exports = {
     const date = new Date(timestamp)
 
     // const year recebe o ano de acordo com o timestamp acima - utilizamos UTC para pegar de forma universal
-    const year = date.getUTCFullYear()
+    const year = date.getFullYear()
 
     // const month recebe o mes de acordo com o timestamp acima - utilizamos UTC para pegar de forma universal
-    const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+    const month = `0${date.getMonth() + 1}`.slice(-2)
 
     // const day recebe o dia de acordo com o timestamp acima - utilizamos UTC para pegar de forma universal
-    const day = `0${date.getUTCDate()}`.slice(-2)
+    const day = `0${date.getDate()}`.slice(-2)
+
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
 
     // retornamos os dados em forma de string na ordem yyyy-mm-dd
     return {
+      day,
+      month,
+      year,
+      hour,
+      minutes,
       iso: `${year}-${month}-${day}`,
       birthDay: `${day}/${month}`,
       format: `${day}/${month}/${year}`
